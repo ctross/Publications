674a5bc08a88b16d8b819734a3722d946c6d60ab
Ctross <ctross@ucdavis.edu>
2016-07-15 11:00:33 -0500

    Update 2.BayesianModelOfPoliceShooting.R

--- PLOS-RacialBiasPoliceShootings/Models/MaintainedImprovedModels/2.BayesianModelOfPoliceShooting.R
+++ PLOS-RacialBiasPoliceShootings/Models/MaintainedImprovedModels/2.BayesianModelOfPoliceShooting.R
@@ -284,6 +284,18 @@ pc95h.RR_Hispanic_Unarmed_Versus_White_Unarmed   <-c()
                                               
 pc95h.RR_Black_Unarmed_Versus_White_Armed        <-c()
 pc95h.RR_Hispanic_Unarmed_Versus_White_Armed     <-c()
+
+m.log.RR_Black_Unarmed_Versus_White_Unarmed[i]  <-c()
+m.log.RR_Hispanic_Unarmed_Versus_White_Unarmed[i] <-c()
+m.log.RR_Black_Unarmed_Versus_White_Armed[i]    <-c()
+m.log.RR_Hispanic_Unarmed_Versus_White_Armed[i]  <-c()
+m.log.RR_Black_Armed_Versus_White_Armed[i]    <-c()
+
+sd.log.RR_Black_Unarmed_Versus_White_Unarmed[i]  <-c()
+sd.log.RR_Hispanic_Unarmed_Versus_White_Unarmed[i]<-c()
+sd.log.RR_Black_Unarmed_Versus_White_Armed[i]    <-c()
+sd.log.RR_Hispanic_Unarmed_Versus_White_Armed[i] <-c()
+sd.log.RR_Black_Armed_Versus_White_Armed[i]   <-c()
  
  
  
